6b188079516365dbd2c7a8ba6eae881a0b5f7a1c
Sahil Rajput <rajputsahil2207@gmail.com>
2026-01-23 15:46:17 +0530

    Started with linear search

---
+++ Searching/Linearsearch.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//return the index of the key if it is found and value of key >5 , then return the double value of key otherwise return the half of the key
+
+int LinearSearch(vector<int>&arr,int key){
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]==key){
+            return  i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int>arr={3,1,6,4,2};
+    int key = 4;
+    int idx = LinearSearch(arr,key);
+    (key>5)?cout<<idx<<" "<<key*2 : cout<<idx<<" "<<key/2;
+}
