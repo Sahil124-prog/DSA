783f08732b79cb9ef110a9a48fc22cd960fac973
sahilrajputx2207-ux <sahilrajputx2207@gmail.com>
2026-01-23 12:58:31 +0530

    Runninsum

---
+++ Arrays/Runningsum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //int n = nums.size();
+    for(int i = 1; i < n; i++){
+        nums[i] = nums[i] + nums[i - 1];
+    }
+    return nums;
+}
+
+
