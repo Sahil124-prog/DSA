bdf72745b1732f177133de4b9bd69f8c9f57638c
Sahil Rajput <rajputsahil2207@gmail.com>
2026-01-23 13:02:22 +0530

    Change in running sum

--- Arrays/Runningsum.cpp
+++ Arrays/Runningsum.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main(){
     //int n = nums.size();
-    for(int i = 1; i < n; i++){
-        nums[i] = nums[i] + nums[i - 1];
-    }
-    return nums;
+    // for(int i = 1; i < n; i++){
+    //     nums[i] = nums[i] + nums[i - 1];
+    // }
+    // return nums;
 }
 
 
